f2197a56a74a561aa71fc41ed2cb9965471cb4e7
Eugene Park <epark215@gmail.com>
2022-11-08 23:03:23 +0000

    Completed refactor of button color function

--- client/app.jsx
+++ client/app.jsx
@@ -30,95 +30,31 @@ export default class App extends React.Component {
         <div className='display-flex flex-wrap'>
           {copy.map(sound => {
             const slice = sound.fileUrl.slice(22);
-            if (sound.soundId === 0 || (sound.soundId - 7) % 7 === 0) {
-              const color = 'red-background';
-              return (
-                <div className='column-third margin-top' key={sound.soundId}>
-                  <div className='display-flex align-center justify-content-center flex-direction-column'>
-                    <button className={`drop-shadow border-radius border-none justify-item-center ${color}`} />
-                    <a className='text-align-center margin-top'>{slice}</a>
-                  </div>
+            const color = (
+              sound.soundId === 0 || (sound.soundId - 7) % 7 === 0
+                ? 'red-background'
+                : sound.soundId === 1 || (sound.soundId - 7) % 7 === 1
+                  ? 'blue-background'
+                  : sound.soundId === 2 || (sound.soundId - 7) % 7 === 2
+                    ? 'purple-background'
+                    : sound.soundId === 3 || (sound.soundId - 7) % 7 === 3
+                      ? 'green-background'
+                      : sound.soundId === 4 || (sound.soundId - 7) % 7 === 4
+                        ? 'yellow-background'
+                        : sound.soundId === 5 || (sound.soundId - 7) % 7 === 5
+                          ? 'orange-background'
+                          : sound.soundId === 6 || (sound.soundId - 7) % 7 === 6
+                            ? 'pink-background'
+                            : 'black-background'
+            );
+            return (
+              <div className='column-third margin-top' key={sound.soundId}>
+                <div className='display-flex align-center justify-content-center flex-direction-column'>
+                  <button className={`drop-shadow border-radius border-none justify-item-center ${color}`}/>
+                  <a className='text-align-center margin-top'>{slice}</a>
                 </div>
-              );
-            } else if (sound.soundId === 1 || (sound.soundId - 7) % 7 === 1) {
-              const color = 'blue-background';
-              return (
-                <div className='column-third margin-top' key={sound.soundId}>
-                  <div className='display-flex align-center justify-content-center flex-direction-column'>
-                    <button className={`drop-shadow border-radius border-none justify-item-center ${color}`} />
-                    <a className='text-align-center margin-top'>{slice}</a>
-                  </div>
-                </div>
-              );
-            } else if (sound.soundId === 2 || (sound.soundId - 7) % 7 === 2) {
-              const color = 'purple-background';
-              return (
-                <div className='column-third margin-top' key={sound.soundId}>
-                  <div className='display-flex align-center justify-content-center flex-direction-column'>
-                    <button className={`drop-shadow border-radius border-none justify-item-center ${color}`} />
-                    <a className='text-align-center margin-top'>{slice}</a>
-                  </div>
-                </div>
-              );
-            } else if (sound.soundId === 3 || (sound.soundId - 7) % 7 === 3) {
-              const color = 'green-background';
-              return (
-                <div className='column-third margin-top' key={sound.soundId}>
-                  <div className='display-flex align-center justify-content-center flex-direction-column'>
-                    <button className={`drop-shadow border-radius border-none justify-item-center ${color}`} />
-                    <a className='text-align-center margin-top'>{slice}</a>
-                  </div>
-                </div>
-              );
-            } else if (sound.soundId === 4 || (sound.soundId - 7) % 7 === 4) {
-              const color = 'yellow-background';
-              return (
-                <div className='column-third margin-top' key={sound.soundId}>
-                  <div className='display-flex align-center justify-content-center flex-direction-column'>
-                    <button className={`drop-shadow border-radius border-none justify-item-center ${color}`} />
-                    <a className='text-align-center margin-top'>{slice}</a>
-                  </div>
-                </div>
-              );
-            } else if (sound.soundId === 5 || (sound.soundId - 7) % 7 === 5) {
-              const color = 'orange-background';
-              return (
-                <div className='column-third margin-top' key={sound.soundId}>
-                  <div className='display-flex align-center justify-content-center flex-direction-column'>
-                    <button className={`drop-shadow border-radius border-none justify-item-center ${color}`} />
-                    <a className='text-align-center margin-top'>{slice}</a>
-                  </div>
-                </div>
-              );
-            } else if (sound.soundId === 6 || (sound.soundId - 7) % 7 === 6) {
-              const color = 'pink-background';
-              return (
-                <div className='column-third margin-top' key={sound.soundId}>
-                  <div className='display-flex align-center justify-content-center flex-direction-column'>
-                    <button className={`drop-shadow border-radius border-none justify-item-center ${color}`} />
-                    <a className='text-align-center margin-top'>{slice}</a>
-                  </div>
-                </div>
-              );
-            } else {
-              const color = 'black-background';
-              return (
-                <div className='column-third margin-top' key={sound.soundId}>
-                  <div className='display-flex align-center justify-content-center flex-direction-column'>
-                    <button className={`drop-shadow border-radius border-none justify-item-center ${color}`} />
-                    <a className='text-align-center margin-top'>{slice}</a>
-                  </div>
-                </div>
-              );
-            }
-            // return (
-            //   <div className='column-third margin-top' key={sound.soundId}>
-            //     <div className='display-flex align-center justify-content-center flex-direction-column'>
-            //       <button className={`drop-shadow border-radius border-none justify-item-center ${color}`}/>
-            //       <a className='text-align-center margin-top'>{slice}</a>
-            //     </div>
-            //   </div>
-            // );
+              </div>
+            );
           })}
         </div>
       </div>
