15023684c760a5dabfe5c6ef6df69d091fe48986
Eugene Park <epark215@gmail.com>
2022-11-08 22:50:08 +0000

    Completed user can view buttons need to refactor

--- client/components/sound-buttons.jsx
+++ client/components/sound-buttons.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function SoundButtons(props) {
   return (
-    <h1 className='text-align-center lucida-sans font-gray'>Sound Buttons</h1>
+    <div>
+      <h2 className='text-align-center lucida-sans font-gray'>Sound Buttons</h2>
+    </div>
   );
 }
