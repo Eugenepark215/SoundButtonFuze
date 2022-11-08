b76ee5eb81b6ad33fb387cc171ad151ce0bca2e6
Eugene Park <epark215@gmail.com>
2022-11-08 23:34:42 +0000

    Completed refactor for color

--- client/app.jsx
+++ client/app.jsx
@@ -28,25 +28,18 @@ export default class App extends React.Component {
       <div>
         <Home />
         <div className='display-flex flex-wrap'>
-          {copy.map(sound => {
+          {copy.map((sound, index) => {
             const slice = sound.fileUrl.slice(22);
-            const color = (
-              sound.soundId === 0 || (sound.soundId - 7) % 7 === 0
-                ? 'red-background'
-                : sound.soundId === 1 || (sound.soundId - 7) % 7 === 1
-                  ? 'blue-background'
-                  : sound.soundId === 2 || (sound.soundId - 7) % 7 === 2
-                    ? 'purple-background'
-                    : sound.soundId === 3 || (sound.soundId - 7) % 7 === 3
-                      ? 'green-background'
-                      : sound.soundId === 4 || (sound.soundId - 7) % 7 === 4
-                        ? 'yellow-background'
-                        : sound.soundId === 5 || (sound.soundId - 7) % 7 === 5
-                          ? 'orange-background'
-                          : sound.soundId === 6 || (sound.soundId - 7) % 7 === 6
-                            ? 'pink-background'
-                            : 'black-background'
-            );
+            const colors = [
+              'red-background',
+              'blue-background',
+              'purple-background',
+              'green-background',
+              'yellow-background',
+              'orange-background',
+              'pink-background',
+              'black-background'];
+            const color = colors[index % colors.length];
             return (
               <div className='column-third margin-top' key={sound.soundId}>
                 <div className='display-flex align-center justify-content-center flex-direction-column'>
