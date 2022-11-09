f83fc58ea8ce14fa812e8d27fb5a3f1d92af6c25
Eugene Park <epark215@gmail.com>
2022-11-09 05:23:18 +0000

    Working on hashroute

--- client/app.jsx
+++ client/app.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Home from './pages/home';
+// import SoundButtonDetail from './pages/sound-button-detail';
+// import { parseRoute } from './lib';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sounds: []
+      sounds: [],
+      current: null
+      // route: parseRoute(window.location.hash)
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -20,6 +25,22 @@ export default class App extends React.Component {
       .then(sound => {
         this.setState({ sounds: sound });
       });
+    // window.addEventListener('hashchange', () => {
+    //   this.setState({ route: parseRoute(window.location.hash) });
+    // });
+    // use anchors href
+  }
+
+  handleClick(event) {
+    if (this.state.current === null) {
+      this.setState({ current: event.target.textContent });
+    } else {
+      this.setState({ current: null });
+    }
+  }
+
+  renderPage() {
+    // render all pages from here and call inside render
   }
 
   render() {
@@ -27,6 +48,7 @@ export default class App extends React.Component {
     return (
       <div>
         <Home />
+        {/* put into home.jsx */}
         <div className='display-flex flex-wrap'>
           {copy.map((sound, index) => {
             const slice = sound.fileUrl.slice(22);
@@ -43,8 +65,8 @@ export default class App extends React.Component {
             return (
               <div className='column-third margin-top' key={sound.soundId}>
                 <div className='display-flex align-center justify-content-center flex-direction-column'>
-                  <button className={`drop-shadow border-radius border-none justify-item-center ${color}`}/>
-                  <a className='text-align-center margin-top'>{slice}</a>
+                  <button className={`w-h70px drop-shadow border-radius-50 border-none justify-item-center ${color}`}/>
+                  <a className='font-gray lucida-sans text-align-center margin-top' onClick={this.handleClick}>{slice}</a>
                 </div>
               </div>
             );
