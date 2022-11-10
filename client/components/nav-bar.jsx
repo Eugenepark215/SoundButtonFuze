4a6b645d1ba5f84c306952b7b3947e687e9248ef
Eugene Park <epark215@gmail.com>
2022-11-10 00:18:05 +0000

    Completed hashroute for navbar header and home icon

--- client/components/nav-bar.jsx
+++ client/components/nav-bar.jsx
@@ -5,11 +5,15 @@ export default function NavBar(props) {
     <div className="container drop-shadow">
       <div className="row cyan-background">
         <div className="column-half padding-left">
-          <h2 id='nav-bar-header'className='white lucida-sans text-align-center'>SoundButtonFuze</h2>
+          <a href='#' className='text-decoration-none'>
+            <h2 id='nav-bar-header' className='white lucida-sans text-align-center'>SoundButtonFuze</h2>
+          </a>
         </div>
         <div id='icon-container' className="column-half row align-center justify-content-center padding-left padding-right">
           <div className="column-third text-align-center">
-            <i className="fa-solid fa-house white" />
+            <a href='#'>
+              <i className="fa-solid fa-house white" />
+            </a>
           </div>
           <div className="column-third text-align-center">
             <i className="fa-solid fa-microphone white" />
