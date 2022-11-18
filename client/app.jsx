88546421b167457e0e8dd3f341074af039e594ed
Eugene Park <epark215@gmail.com>
2022-11-18 22:07:54 +0000

    Completed user can see oscilator

--- client/app.jsx
+++ client/app.jsx
@@ -62,9 +62,9 @@ export default class App extends React.Component {
   }
 
   render() {
-    const { user } = this.state;
+    const { user, route } = this.state;
     const { handleSignIn } = this;
-    const contextValue = { user, handleSignIn };
+    const contextValue = { user, handleSignIn, route };
     return (
       <div>
         <AppContext.Provider value={contextValue}>
