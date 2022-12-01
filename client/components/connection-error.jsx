7eaff10b22c557dcc9e828343575978d58c6922d
Eugene Park <epark215@gmail.com>
2022-12-01 22:00:54 +0000

    Refactored connection-error as function

--- client/components/connection-error.jsx
+++ client/components/connection-error.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 
-export default class ConnectionError extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      something: null
-    };
-  }
-
-  render() {
-    return (
-      <div className="error-message">
-        <div className="modal-container">
-          <h1 className="title-header lucida-sans">Sorry, there was an error connecting to the network! Please check your internet connection and try again.</h1>
-        </div>
+export default function ConnectionError() {
+  return (
+    <div className="error-message">
+      <div className="modal-container">
+        <h1 className="title-header lucida-sans">Sorry, there was an error connecting to the network! Please check your internet connection and try again.</h1>
       </div>
-    );
-  }
+    </div>
+  );
 }
